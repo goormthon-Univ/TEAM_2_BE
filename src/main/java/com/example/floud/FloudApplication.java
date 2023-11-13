6c7075d2febde3011d178f46c30545b9f4764eb1
Seongjoon-K <jobcho6320@naver.com>
2023-11-13 11:57:06 +0900

    chore: 스프링 개발 환경 셋팅

---
+++ src/main/java/com/example/floud/FloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.floud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloudApplication.class, args);
+	}
+
+}
