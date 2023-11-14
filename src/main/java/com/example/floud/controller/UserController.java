750f676d68d3dae158852dc3f7036e5b74b17e09
Seongjoon-K <jobcho6320@naver.com>
2023-11-14 11:15:03 +0900

    feat: 유저 ID 조회 메소드 구현

--- src/main/java/com/example/floud/controller/UserController.java
+++ src/main/java/com/example/floud/controller/UserController.java
@@ -1,5 +1,28 @@
 package com.example.floud.controller;
 
+import com.example.floud.entity.User;
+import com.example.floud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // 유저 ID로 사용자를 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(
+            @PathVariable Long id) {
+        return userService.getUserById(id)
+                .map(user-> ResponseEntity.ok(user))
+                .orElse(ResponseEntity.notFound().build());
+    }
 
 }
--- src/main/java/com/example/floud/service/UserService.java
+++ src/main/java/com/example/floud/service/UserService.java
@@ -1,4 +1,19 @@
 package com.example.floud.service;
 
+import com.example.floud.entity.User;
+import com.example.floud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> getUserById(Long userId) {
+        return userRepository.findById(userId);
+    }
 }
