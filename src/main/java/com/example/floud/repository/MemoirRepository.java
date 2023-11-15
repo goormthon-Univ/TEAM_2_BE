54e3abb6ef1058879877f360c969460ffd1ac9ad
Seongjoon-K <jobcho6320@naver.com>
2023-11-15 19:09:53 +0900

    feat: 회고 단건 조회 인터페이스 구현

---
+++ src/main/java/com/example/floud/repository/MemoirRepository.java
@@ -0,0 +1,13 @@
+package com.example.floud.repository;
+
+import com.example.floud.entity.Memoir;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemoirRepository extends JpaRepository<Memoir, Long> {
+
+    Optional<Memoir> findByMemoirId(Long memoirId);
+
+}
