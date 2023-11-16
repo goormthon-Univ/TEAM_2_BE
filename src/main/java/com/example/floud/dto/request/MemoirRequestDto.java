f2c1fba9248e7c1b54cbd44dd948678c6a2ca69e
Seongjoon-K <jobcho6320@naver.com>
2023-11-16 23:59:05 +0900

    feat: MemoirResquestDto에 빌더 메소드 추가

--- src/main/java/com/example/floud/dto/request/MemoirRequestDto.java
+++ src/main/java/com/example/floud/dto/request/MemoirRequestDto.java
@@ -1,15 +1,29 @@
 package com.example.floud.dto.request;
 
 
+import com.example.floud.entity.Memoir;
+import com.example.floud.entity.User;
+import com.example.floud.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class MemoirRequestDto {
+    private Long user_id;
     private String title;
     private String memoirKeep;
     private String memoirProblem;
     private String memoirTry;
+
+    public Memoir toMemoir(User user) {
+        return Memoir.builder()
+                .title(title)
+                .memoirKeep(memoirKeep)
+                .memoirProblem(memoirProblem)
+                .memoirTry(memoirTry)
+                .user(user)
+                .build();
+    }
 }
