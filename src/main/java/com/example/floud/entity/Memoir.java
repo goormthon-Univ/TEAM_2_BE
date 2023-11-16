ac3e3b1f63a1475122511bc213dc94fc89171f28
Seongjoon-K <jobcho6320@naver.com>
2023-11-17 05:34:11 +0900

    chore: gitignore 추가

--- src/main/java/com/example/floud/entity/Memoir.java
+++ src/main/java/com/example/floud/entity/Memoir.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
