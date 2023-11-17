8966c50967405c64edd0527c1691767f092bff5c
Seongjoon-K <jobcho6320@naver.com>
2023-11-17 22:02:30 +0900

    fix: addCorsMappings 추가

--- src/main/java/com/example/floud/config/CorsFilterConfig.java
+++ src/main/java/com/example/floud/config/CorsFilterConfig.java
@@ -1,26 +1,26 @@
-package com.example.floud.config;
-
-import lombok.Getter;
-import org.apache.catalina.filters.CorsFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-@Configuration
-@Getter
-public class CorsFilterConfig {
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter();
-    }
-
-}
+//package com.example.floud.config;
+//
+//import lombok.Getter;
+//import org.apache.catalina.filters.CorsFilter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//
+//@Configuration
+//@Getter
+//public class CorsFilterConfig {
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOrigin("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter();
+//    }
+//
+//}
