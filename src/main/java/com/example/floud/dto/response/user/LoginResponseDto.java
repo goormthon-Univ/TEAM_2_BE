e7005284473185753038f79488c64f6f401fad44
kang <kang6@ewhain.net>
2023-11-18 20:27:17 +0900

    refactor: 사용하지 않는 코드 제거

--- src/main/java/com/example/floud/dto/response/user/LoginResponseDto.java
+++ src/main/java/com/example/floud/dto/response/user/LoginResponseDto.java
@@ -3,33 +3,10 @@ package com.example.floud.dto.response.user;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
 public class LoginResponseDto {
     private Long user_id;
-//    private String message;
-//    private Data data;
-
-//    public LoginResponseDto(String message, Data data) {
-//        this.message = message;
-//        this.data = data;
-//    }
-//
-//    @Getter
-//    public static class Data {
-//        private Long userId;
-//        private String accessToken;
-//        private String refreshToken;
-//
-//
-//        public Data(Long userId, String accessToken, String refreshToken) {
-//            this.userId = userId;
-//            this.accessToken = accessToken;
-//            this.refreshToken = refreshToken;
-//        }
-//
-//    }
 }
